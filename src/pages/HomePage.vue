27801122e872211b43b6077624caa28ea8f13322
Dmitrii <dimau777@gmail.com>
2024-01-08 18:22:54 +0400

    feat: add page title for the home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,15 @@
 <template>
+  <h1 class="page-title">Best employees of the month</h1>
   <PersonsList />
 </template>
 
 <script setup lang="ts">
 import { PersonsList } from "../widgets/persons-list";
 </script>
+
+<style scoped>
+.page-title {
+  text-align: center;
+  margin-bottom: 2em;
+}
+</style>
